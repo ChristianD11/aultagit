bc9ea3fc064728ba7eaeeb8d92b12f91546cac88
ChristianDuarte <christian_souza92@hotmail.com>
2021-05-05 12:50:37 -0300

    aula 03

---
+++ Aula 3/Exercicios/Exercicio01.java
@@ -0,0 +1,16 @@
+public class Exercicio01 {
+    public static void main(String[] args) {
+        meusDados();
+    }
+
+    static void meusDados() {
+        System.out.println("---------------");
+        System.out.println("Christian / 29");
+        System.out.println("Santos");
+        System.out.println("---------------");
+    }
+    
+}
+
+    
+
